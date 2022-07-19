aba2037473353c4be4bb167d58b5ebe338350ade
Stephanie smith <smithse4@gmail.com>
2022-07-19 11:30:11 -0400

    fiixed local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,7 +85,7 @@ let mst = document.querySelector("#mst");
 mh1.addEventListener("input", function () {
   mt1.value = JSON.parse(mh1.value) * jobOneRate;
   mst.value = JSON.parse(mt1.value) + JSON.parse(mt2.value);
-  localStorage.setItem('mh1', JSON.stringify(mh1.value));
+  localStorage.setItem("mh1", JSON.stringify(mh1.value));
   dailyTotalsJ1();
   summaryTotals();
 });
@@ -93,7 +93,7 @@ mh1.addEventListener("input", function () {
 mh2.addEventListener("input", function () {
   mt2.value = JSON.parse(mh2.value) * jobTwoRate;
   mst.value = JSON.parse(mt1.value) + JSON.parse(mt2.value);
-  localStorage.setItem('mh2', JSON.stringify(mh2.value));
+  localStorage.setItem("mh2", JSON.stringify(mh2.value));
   dailyTotalsJ2();
   summaryTotals();
 });
@@ -108,7 +108,7 @@ let tst = document.querySelector("#tst");
 th1.addEventListener("input", function () {
   tt1.value = JSON.parse(th1.value) * jobOneRate;
   tst.value = JSON.parse(tt1.value) + JSON.parse(tt2.value);
-  localStorage.setItem('th1', JSON.stringify(th1.value));
+  localStorage.setItem("th1", JSON.stringify(th1.value));
   dailyTotalsJ1();
   summaryTotals();
 });
@@ -116,7 +116,7 @@ th1.addEventListener("input", function () {
 th2.addEventListener("input", function () {
   tt2.value = JSON.parse(th2.value) * jobTwoRate;
   tst.value = JSON.parse(tt1.value) + JSON.parse(tt2.value);
-  localStorage.setItem('th2', JSON.stringify(th2.value));
+  localStorage.setItem("th2", JSON.stringify(th2.value));
   dailyTotalsJ2();
   summaryTotals();
 });
@@ -131,7 +131,7 @@ let wst = document.querySelector("#wst");
 wh1.addEventListener("input", function () {
   wt1.value = JSON.parse(wh1.value) * jobOneRate;
   wst.value = JSON.parse(wt1.value) + JSON.parse(wt2.value);
-  localStorage.setItem('wh1', JSON.stringify(wh1.value));
+  localStorage.setItem("wh1", JSON.stringify(wh1.value));
   dailyTotalsJ1();
   summaryTotals();
 });
@@ -139,7 +139,7 @@ wh1.addEventListener("input", function () {
 wh2.addEventListener("input", function () {
   wt2.value = JSON.parse(wh2.value) * jobTwoRate;
   wst.value = JSON.parse(wt1.value) + JSON.parse(wt2.value);
-  localStorage.setItem('wh2', JSON.stringify(wh2.value));
+  localStorage.setItem("wh2", JSON.stringify(wh2.value));
   dailyTotalsJ2();
   summaryTotals();
 });
@@ -154,7 +154,7 @@ let thst = document.querySelector("#thst");
 thh1.addEventListener("input", function () {
   tht1.value = JSON.parse(thh1.value) * jobOneRate;
   thst.value = JSON.parse(tht1.value) + JSON.parse(tht2.value);
-  localStorage.setItem('thh1', JSON.stringify(thh1.value));
+  localStorage.setItem("thh1", JSON.stringify(thh1.value));
   dailyTotalsJ1();
   summaryTotals();
 });
@@ -162,7 +162,7 @@ thh1.addEventListener("input", function () {
 thh2.addEventListener("input", function () {
   tht2.value = JSON.parse(thh2.value) * jobTwoRate;
   thst.value = JSON.parse(tht1.value) + JSON.parse(tht2.value);
-  localStorage.setItem('thh2', JSON.stringify(thh2.value));
+  localStorage.setItem("thh2", JSON.stringify(thh2.value));
   dailyTotalsJ2();
   summaryTotals();
 });
@@ -177,7 +177,7 @@ let fst = document.querySelector("#fst");
 fh1.addEventListener("input", function () {
   ft1.value = JSON.parse(fh1.value) * jobOneRate;
   fst.value = JSON.parse(ft1.value) + JSON.parse(ft2.value);
-  localStorage.setItem('fh1', JSON.stringify(fh1.value));
+  localStorage.setItem("fh1", JSON.stringify(fh1.value));
   dailyTotalsJ1();
   summaryTotals();
 });
@@ -185,7 +185,7 @@ fh1.addEventListener("input", function () {
 fh2.addEventListener("input", function () {
   ft2.value = JSON.parse(fh2.value) * jobTwoRate;
   fst.value = JSON.parse(ft1.value) + JSON.parse(ft2.value);
-  localStorage.setItem('fh2', JSON.stringify(fh2.value));
+  localStorage.setItem("fh2", JSON.stringify(fh2.value));
   dailyTotalsJ2();
   summaryTotals();
 });
@@ -198,25 +198,25 @@ function summaryTotals() {
 }
 
 function dailyTotalsJ1() {
-    addH1.value =
-      JSON.parse(mh1.value) +
-      JSON.parse(th1.value) +
-      JSON.parse(wh1.value) +
-      JSON.parse(thh1.value) +
-      JSON.parse(fh1.value);
-    addT1.value =
-      JSON.parse(mt1.value) +
-      JSON.parse(tt1.value) +
-      JSON.parse(wt1.value) +
-      JSON.parse(tht1.value) +
-      JSON.parse(ft1.value);
-    addST.value =
-      JSON.parse(mst.value) +
-      JSON.parse(tst.value) +
-      JSON.parse(wst.value) +
-      JSON.parse(thst.value) +
-      JSON.parse(fst.value);
-  }
+  addH1.value =
+    JSON.parse(mh1.value) +
+    JSON.parse(th1.value) +
+    JSON.parse(wh1.value) +
+    JSON.parse(thh1.value) +
+    JSON.parse(fh1.value);
+  addT1.value =
+    JSON.parse(mt1.value) +
+    JSON.parse(tt1.value) +
+    JSON.parse(wt1.value) +
+    JSON.parse(tht1.value) +
+    JSON.parse(ft1.value);
+  addST.value =
+    JSON.parse(mst.value) +
+    JSON.parse(tst.value) +
+    JSON.parse(wst.value) +
+    JSON.parse(thst.value) +
+    JSON.parse(fst.value);
+}
 
 function dailyTotalsJ2() {
   addH2.value =
@@ -240,20 +240,69 @@ function dailyTotalsJ2() {
 }
 
 function displayStoredData() {
-    mh1.value = JSON.parse(localStorage.getItem('fh2'));
-    th1.value = JSON.parse(localStorage.getItem('th1'));
-    wh1.value = JSON.parse(localStorage.getItem('wh1'));
-    thh1.value = JSON.parse(localStorage.getItem('thh1'));
-    fh1.value = JSON.parse(localStorage.getItem('fh1'));
-    mh2.value = JSON.parse(localStorage.getItem('mh2'));
-    th2.value = JSON.parse(localStorage.getItem('th2'));
-    wh2.value = JSON.parse(localStorage.getItem('wh2'));
-    thh2.value = JSON.parse(localStorage.getItem('thh2'));
-    fh2.value = JSON.parse(localStorage.getItem('fh2'));
+  mh1.value = JSON.parse(localStorage.getItem("fh2")) || 0;
+  th1.value = JSON.parse(localStorage.getItem("th1")) || 0;
+  wh1.value = JSON.parse(localStorage.getItem("wh1")) || 0;
+  thh1.value = JSON.parse(localStorage.getItem("thh1")) || 0;
+  fh1.value = JSON.parse(localStorage.getItem("fh1")) || 0;
+  mh2.value = JSON.parse(localStorage.getItem("mh2")) || 0;
+  th2.value = JSON.parse(localStorage.getItem("th2")) || 0;
+  wh2.value = JSON.parse(localStorage.getItem("wh2")) || 0;
+  thh2.value = JSON.parse(localStorage.getItem("thh2")) || 0;
+  fh2.value = JSON.parse(localStorage.getItem("fh2")) || 0;
 
-    summaryTotals();
-    dailyTotalsJ1();
-    dailyTotalsJ2();
+  summaryTotals();
+  dailyTotalsJ1();
+  dailyTotalsJ2();
 }
 
-// displayStoredData();
+displayStoredData();
+summaryTotals();
+dailyTotalsJ1();
+dailyTotalsJ2();
+
+function onLoad() {
+  // Pull data from localStorage
+  displayStoredData();
+
+  // DAILY CALCULATIONS
+  // MONDAY
+  mt1.value = JSON.parse(mh1.value) * jobOneRate;
+  mst.value = JSON.parse(mt1.value) + JSON.parse(mt2.value);
+  mt2.value = JSON.parse(mh2.value) * jobTwoRate;
+  mst.value = JSON.parse(mt1.value) + JSON.parse(mt2.value);
+
+  // TUESDAY
+  tt1.value = JSON.parse(th1.value) * jobOneRate;
+  tst.value = JSON.parse(tt1.value) + JSON.parse(tt2.value);
+  tt2.value = JSON.parse(th2.value) * jobTwoRate;
+  tst.value = JSON.parse(tt1.value) + JSON.parse(tt2.value);
+
+  // WEDNESDAY
+  wt1.value = JSON.parse(wh1.value) * jobOneRate;
+  wst.value = JSON.parse(wt1.value) + JSON.parse(wt2.value);
+  wt2.value = JSON.parse(wh2.value) * jobTwoRate;
+  wst.value = JSON.parse(wt1.value) + JSON.parse(wt2.value);
+
+  // THURSDAY
+  tht1.value = JSON.parse(thh1.value) * jobOneRate;
+  thst.value = JSON.parse(tht1.value) + JSON.parse(tht2.value);
+  tht2.value = JSON.parse(thh2.value) * jobTwoRate;
+  thst.value = JSON.parse(tht1.value) + JSON.parse(tht2.value);
+
+  // FRIDAY
+  ft1.value = JSON.parse(fh1.value) * jobOneRate;
+  fst.value = JSON.parse(ft1.value) + JSON.parse(ft2.value);
+  ft2.value = JSON.parse(fh2.value) * jobTwoRate;
+  fst.value = JSON.parse(ft1.value) + JSON.parse(ft2.value);
+
+  // TOTALS & SUMMARIES
+  // Job 1 daily totals
+  dailyTotalsJ1();
+  // Job 2 daily totals
+  dailyTotalsJ2();
+  // Update summary totals - Income & Hours
+  summaryTotals();
+}
+
+onLoad();
